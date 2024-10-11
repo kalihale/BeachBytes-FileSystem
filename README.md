113fdfe3fea0437e0fa1e7e5f3706d95e3b73b53
vikaskalagi <vikaskalagi123@gmail.com>
2024-10-11 03:48:13 +0000

    compiled using CMake

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # BeachBytes
 CS270 Project
+
+
+
+To Comiple
+
+mkdir build
+cd build
+cmake ..
+make
+./cs270
---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "test.h"
+
+void printHello(){
+	std::cout<<"Hello World\n";
+	return;
+}
+int main() {
+    printHello();
+    return 0;
+}
